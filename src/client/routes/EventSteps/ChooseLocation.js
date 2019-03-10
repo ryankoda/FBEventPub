09013884016e3306cbada26d12ed0d8c61ed97d3
RYAN KODA <kodadota@gmail.com>
2019-03-09 23:04:24 -0800

    Update ChooseLocation.js

--- src/client/routes/EventSteps/ChooseLocation.js
+++ src/client/routes/EventSteps/ChooseLocation.js
@@ -3,11 +3,6 @@ import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import AutocompleteComponent from './Asset/GeocodeAutocomplete';
 import GoogleMapComponent from './Asset/GoogleMapOption';
 import { saveLocalStorage } from '../../../utils/localstorage';
 
@@ -22,19 +17,6 @@ class ChooseLocation extends React.Component {
     super(props);
   }
 
-/*
-  locationLink(address, lat, lon) {
-    // console.log('address: ' + address + '\nlat: ' + lat + '\nlon: ' + lon);
-
-    saveLocalStorage('address', address);
-    saveLocalStorage('lat', lat);
-    saveLocalStorage('lon', lon);
-
-    // saveLocalStorage('coordinates', JSON.stringify(coordinates));
-    // saveLocalStorage('viewport',JSON.stringify(viewport));
-  }
-*/
-
   render() {
     const { classes } = this.props;
     return (
@@ -44,11 +26,8 @@ class ChooseLocation extends React.Component {
 
           <Card className={classes.card}>
             <Grid container spacing={24}>
-              {/*<Grid item xs={12}>
-                <AutocompleteComponent locationLink={this.locationLink} />
-              </Grid>*/}
               <Grid item xs={12}>
-                <GoogleMapComponent locationLink={this.locationLink} />
+                <GoogleMapComponent />
               </Grid>
             </Grid>
 
